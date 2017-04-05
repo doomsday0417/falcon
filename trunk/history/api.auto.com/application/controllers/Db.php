a992a1ff064fda52dd24ba4ade77705dc505c748
doomsday <365448848@qq.com>
2017-04-05 18:22:13 +0800

    数据库监控API优化

--- trunk/history/api.auto.com/application/controllers/Db.php
+++ trunk/history/api.auto.com/application/controllers/Db.php
@@ -6,21 +6,18 @@ class DbController extends Aomp_Yaf_Controller_Api
     {
         $data = $this->getRequest()->getRequest();
 
-        /* @var $log Aomp_Application_Resource_Log */
-        $log = Aomp_Yaf_ResourceManager::getResource('log');
-        $log->setLog($data['ip']);
 
-        $dbModel = new Model_Remote_Db();
+        $dbModel = new Model_Remote_Dba();
 
         try {
-            $dbModel->addDbData($data);
+            $dbModel->addDba($data);
         }catch (Model_Exception $e){
             /* @var $log Aomp_Application_Resource_Log */
             $log = Aomp_Yaf_ResourceManager::getResource('log');
             $log->setLog("Dispatch error with code:{$e->getCode()} message: {$e->getMessage()} on {$e->getFile()} @ {$e->getLine()}");
         }
 
-        $this->json(true, '接受成功');
+        $this->json(true, '接收成功');
 
     }
 }
